e41fa50d943e9da3074f00a5f9b6ccab723a5d17
Kemal Yılmaz <kemalylmz@gmail.com>
2024-10-17 17:13:48 +0300

    Removed HTTPS check and added detailed comments

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,29 +1,31 @@
 import os
 import logging
-import re
 
 class Config:
+    # Load the API URL from environment variables or use a default value
     api_url = os.getenv('API_URL', "https://v3.football.api-sports.io")
-    api_key = os.getenv('API_KEY')
-    db_path = os.getenv('DATABASE_URL', 'sqlite:///database.db')  # SQLite database path
+    
+    # Load the API key from environment variables; provide a default key for development
+    api_key = os.getenv('API_KEY', 'your_test_api_key')
+    
+    # Load the database path from environment variables or use a default SQLite database
+    db_path = os.getenv('DATABASE_URL', 'sqlite:///database.db')
 
     @staticmethod
     def check_env_variables():
+        # Check if the API URL is set
         if not Config.api_url:
             logging.error("Missing API URL environment variable. Please check the environment variables.")
             raise ValueError("Missing API URL environment variable.")
-        
-        if not re.match(r'^https?://', Config.api_url):
-            logging.error("Invalid API URL format.")
-            raise ValueError("Invalid API URL format.")
-
         logging.info("API URL successfully read.")
 
+        # Check if the API key is set
         if not Config.api_key:
             logging.error("Missing API Key environment variable. Please check the environment variables.")
             raise ValueError("Missing API Key environment variable.")
         logging.info("API Key successfully read.")
 
+        # Check if the database path is set
         if not Config.db_path:
             logging.error("Missing database path environment variable. Please check the environment variables.")
             raise ValueError("Missing database path environment variable.")
