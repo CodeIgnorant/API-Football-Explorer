2cdc4a91a902c25d6c8eaed490b2e4f1fe272936
Kemal Yilmaz <kemalylmz@gmail.com>
2024-10-19 16:13:08 +0300

    refactor: remove unused lines in init.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,3 @@
 from .home_controller import *  # Home controller
 from .countries_controller import *  # Controller related to countries
-from .test_api_controller import *  # Controller related to testing the API
 from .leagues_controller import *  # Controller related to leagues
